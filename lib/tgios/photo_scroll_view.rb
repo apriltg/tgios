7fefdc51a811df7061d20d15aaeb0b9e3397d505
April Tsang <april@tofugear.com>
2014-02-26 17:47:58 +0800

    refactor photo scroll view

--- lib/tgios/photo_scroll_view.rb
+++ lib/tgios/photo_scroll_view.rb
@@ -1,17 +1,31 @@
 module Tgios
   class PhotoScrollView < UIScrollView
+    attr_accessor :image
     MAX_SCALE = 4.0
 
-    def initWithFrame(frame, image: image)
-      initWithFrame(frame)
-      ap "init #{self.class.name}"
-      self.showsVerticalScrollIndicator = false
-      self.showsHorizontalScrollIndicator = false
-      self.bouncesZoom = true
-      self.decelerationRate = UIScrollViewDecelerationRateFast
-      self.delegate = self
-      self.backgroundColor = :clear.uicolor
+    def init
+      if super
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+        self.bouncesZoom = true
+        self.decelerationRate = UIScrollViewDecelerationRateFast
+        self.delegate = self
+        self.backgroundColor = :clear.uicolor
+      end
+      self
+    end
 
+    def initWithFrame(frame, image: image)
+      init
+      self.frame = frame
+      self.image = image
+
+      self
+    end
+
+    def image=(image)
+      super
+      frame = self.frame
       page_rect = CGRectMake(0, 0, image.size.width, image.size.height)
 
       img_scale = frame.size.width / page_rect.size.width
@@ -30,21 +44,21 @@ module Tgios
 
       self.maximumZoomScale = MAX_SCALE / img_scale
       self.minimumZoomScale = fit_scale / img_scale
-
-      self
     end
 
     def layoutSubviews
       super
+      unless @image_view.nil?
+        bsize = self.bounds.size
+        center_frame = @image_view.frame
 
-      bsize = self.bounds.size
-      center_frame = @image_view.frame
+        center_frame.origin.x = center_frame.size.width < bsize.width ? (bsize.width - center_frame.size.width) / 2.0 : 0
+        center_frame.origin.y = center_frame.size.height < bsize.height ? (bsize.height - center_frame.size.height) / 2.0 : 0
 
-      center_frame.origin.x = center_frame.size.width < bsize.width ? (bsize.width - center_frame.size.width) / 2.0 : 0
-      center_frame.origin.y = center_frame.size.height < bsize.height ? (bsize.height - center_frame.size.height) / 2.0 : 0
+        @image_view.frame = center_frame
+        @image_view.contentScaleFactor = 1.0
 
-      @image_view.frame = center_frame
-      @image_view.contentScaleFactor = 1.0
+      end
 
     end
 
