b0b1b8ace12690f51c2ba181bc61f45cac1fe2bc
April Tsang <april@tofugear.com>
2014-06-13 19:11:42 +0800

    modify nsdate helper to handle date string with mini-seconds

--- lib/tgios/ns_date_helper.rb
+++ lib/tgios/ns_date_helper.rb
@@ -1,22 +1,25 @@
 module Tgios
   class NSDateHelper
     def self.to_nsdate(date_string)
-      @formatter ||= (
-      @formatter = NSDateFormatter.new
-      @formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-      @formatter.timeZone = NSTimeZone.timeZoneForSecondsFromGMT(0)
-      @formatter
-      )
-      date = @formatter.dateFromString(date_string)
-      if date.nil?
-        @formatter2 ||= (
-        @formatter2 = NSDateFormatter.new
-        @formatter2.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        @formatter2.timeZone = NSTimeZone.timeZoneForSecondsFromGMT(0)
-        @formatter2
+      if date_string.is_a?(String)
+        date_string = date_string.gsub(/\.\d*/, '') # remove mini-seconds
+        @formatter ||= (
+        @formatter = NSDateFormatter.new
+        @formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        @formatter.timeZone = NSTimeZone.timeZoneForSecondsFromGMT(0)
+        @formatter
         )
-        date = @formatter2.dateFromString(date_string).utc
-
+        date = @formatter.dateFromString(date_string)
+        if date.nil?
+          @formatter2 ||= (
+          @formatter2 = NSDateFormatter.new
+          @formatter2.dateFormat = "yyyy-MM-dd HH:mm:ss"
+          @formatter2.timeZone = NSTimeZone.timeZoneForSecondsFromGMT(0)
+          @formatter2
+          )
+          date = @formatter2.dateFromString(date_string).utc
+        end
+        date
       else
         date.utc
       end
